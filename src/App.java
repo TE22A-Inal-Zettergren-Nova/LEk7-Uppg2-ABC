080a2f709de3440342be5c1dd854c1a0f7e6f846
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-16 09:24:57 +0200

    Skrivit ut koden

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        /* Uppgift a*/
+        System.out.println("Koden funkade inte för att stortTal variabeln inte är utanför if statementet");
+
+        int litetTal=10;
+        if(litetTal<1000)
+        {
+            int stortTal=1000;
+            System.out.println(litetTal +"är mindre än"+stortTal);
+        }
+        else
+        {
+            System.out.println(litetTal+"är mindre än"+stortTal);
+        }
+            System.out.println("litetTal="+litetTal+",stortTal="+stortTal);
+
     }
 }
