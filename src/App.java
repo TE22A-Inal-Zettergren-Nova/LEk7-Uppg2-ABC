be61ea2dc109d5ecf11d924cf16bef284a859ba7
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-16 09:27:46 +0200

    flytta stortal, så programmet funkar

--- src/App.java
+++ src/App.java
@@ -2,19 +2,23 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         /* Uppgift a*/
-        System.out.println("Koden funkade inte för att stortTal variabeln inte är utanför if statementet");
+        /* Koden funkade inte för att stortTal variabeln inte är utanför if statementet*/
+        /*Flyttar stortal så att den är utanför if statmentet*/
 
         int litetTal=10;
+        int stortTal=1000;
         if(litetTal<1000)
         {
-            int stortTal=1000;
-            System.out.println(litetTal +"är mindre än"+stortTal);
+           
+            System.out.println(litetTal +" är mindre än "+stortTal);
         }
         else
         {
-            System.out.println(litetTal+"är mindre än"+stortTal);
+            System.out.println(litetTal+" är mindre än "+stortTal);
         }
-            System.out.println("litetTal="+litetTal+",stortTal="+stortTal);
+            System.out.println("litetTal= "+litetTal+",stortTal= "+stortTal);
 
     }
 }
+
+        /*Uppgift b */
